40e5631b6b483c6e5233225fa48d05720f37c688
Anthony Morada <anthony.omorada@gmail.com>
2020-03-19 09:30:29 -0400

    Updated Script and Added Report
    Updated daily script to accomidate new data format. Added a report which includes data analysis

--- update-master-script.R
+++ update-master-script.R
@@ -4,13 +4,28 @@
 setwd("~/Documents/GitHub/tcm-quicc-project")
 #pulling in master
 library(readxl)
-master <- read_excel("~/Documents/safedata/tcm-quicc/master.xlsx", col_types = c("text", "text", "date", "text", "date", "date", "date", "text", "date", "date", "text", "text", "numeric", "text", "text"))
+master <- read_excel("~/Documents/safedata/tcm-quicc/master.xlsx", col_types = c("text", "text", "date", "text", "date", "date", "date", "text", "date", "date", "text", "text", "numeric", "text", "text", "text"))
 
 #pulling in daily report
-update <- read_excel("~/Documents/safedata/tcm-quicc/dailyreport.xls", col_types = c("text", "text", "date", "text", "date", "date", "date", "text", "date", "date", "text", "text", "numeric", "text", "text"), skip = 2)
+update <- read_excel("~/Documents/safedata/tcm-quicc/dailyreport.xls", col_types = c("text", "text", "date", "text", "date", "date", "date", "date", "text", "date", "date", "text","text", "numeric", "text", "text", "text"), skip = 2)
+
 update <- subset(update, !is.na(update$`Admit Date`))
+for(i in 1: nrow(update)){
+  if (is.na(update$`PC/Cardiology Appt
+(1-7)`[i] && !is.na(update$`PC/Cardiology Appt
+(8-14)`[i]))){
+    update$`PC/Cardiology Appt
+(1-7)`[i] <- update$`PC/Cardiology Appt
+(8-14)`[i]
+  } 
+}
+
+update <- update[-7] ## Removing 8-14 column
+
 colnames(update) <- colnames(master)
 update$`TCM Order` <- !is.na(update$`TCM Order`) 
+update$`Follow up With__ Order` <- !is.na(update$`Follow up With__ Order`)
+rm(i)
 
 library(openxlsx)
 master <- rbind(master,update) #bind
@@ -38,15 +53,15 @@ for (i in 1:length(unique(as.Date(master$Discharged)))) {
   fill$`Total Discharge to home` <- sum(grepl("home", sub$Disposition, ignore.case = T))
   fill$`TCM Order for Home or Self Care Disposition` <- sum(sub$`TCM Order` == TRUE & grepl("home", sub$Disposition, ignore.case = T))
   fill$`% TCM Order Placed Appropriately (Expect 100%)` <- fill$`TCM Order for Home or Self Care Disposition`/fill$`Total Discharge to home` * 100
-  fill$`Total TCM Appts made for home dischrg` <- sum(grepl("home", sub$Disposition, ignore.case = T) & !is.na(sub$`TCM (Any Specialty)`))
+  fill$`Total TCM Appts made for home dischrg` <- sum(grepl("home", sub$Disposition, ignore.case = T) & !is.na(sub$`TCM Appt Date (Within 2 Weeks)`))
   fill$`% TCM Appt Scheduled for Home or Self Care (Expect 100%)` <- fill$`Total TCM Appts made for home dischrg`/fill$`Total Discharge to home` * 100
   fill$`Total TCM's Scheduled for SNF (Expect 0)` <- sum(!grepl("home", sub$Disposition, ignore.case = T) & sub$`TCM Order` == TRUE)
   
   # subset here for home patients only with TCM appointments
-  sub <- sub[grepl("home", sub$Disposition, ignore.case = T) & !is.na(sub$`TCM (Any Specialty)`),]
-  fill$`% scheduled within 7 days` <- sum(as.numeric(as.Date(sub$`TCM (Any Specialty)`)-as.Date(sub$Discharged)) <= 7)/fill$`Total TCM Appts made for home dischrg` * 100
-  fill$`% scheduled iwthin 8-14 days` <- sum(as.Date(sub$`TCM (Any Specialty)`)-as.Date(sub$Discharged) <= 14 & as.Date(sub$`TCM (Any Specialty)`)-as.Date(sub$Discharged) > 7)/fill$`Total TCM Appts made for home dischrg` * 100
-  fill$`%scheduled within 14 days` <- sum(as.Date(sub$`TCM (Any Specialty)`)-as.Date(sub$Discharged) <= 14)/fill$`Total TCM Appts made for home dischrg` * 100
+  sub <- sub[grepl("home", sub$Disposition, ignore.case = T) & !is.na(sub$`TCM Appt Date (Within 2 Weeks)`),]
+  fill$`% scheduled within 7 days` <- sum(as.numeric(as.Date(sub$`TCM Appt Date (Within 2 Weeks)`)-as.Date(sub$Discharged)) <= 7)/fill$`Total TCM Appts made for home dischrg` * 100
+  fill$`% scheduled iwthin 8-14 days` <- sum(as.Date(sub$`TCM Appt Date (Within 2 Weeks)`)-as.Date(sub$Discharged) <= 14 & as.Date(sub$`TCM Appt Date (Within 2 Weeks)`)-as.Date(sub$Discharged) > 7)/fill$`Total TCM Appts made for home dischrg` * 100
+  fill$`%scheduled within 14 days` <- sum(as.Date(sub$`TCM Appt Date (Within 2 Weeks)`)-as.Date(sub$Discharged) <= 14)/fill$`Total TCM Appts made for home dischrg` * 100
   
   #rbinding to monitor
   monitor <- rbind(monitor,fill)
@@ -58,8 +73,6 @@ rm(sub)
 rm(fill)
 
 write.xlsx(monitor,"~/Documents/safedata/tcm-quicc/monitor.xlsx")
-
-
 # graph for monitor data
 library(ggplot2)
 
@@ -70,11 +83,22 @@ ggplot(monitor, aes(`Discharge Date`, `% TCM Appt Scheduled for Home or Self Car
   geom_point() +
   geom_line()+
   geom_smooth(method = "lm", se = FALSE)
-ggsave("TCM-apt-schedule.pdf")
+ggsave("~/Documents/safedata/tcm-quicc/TCM-apt-schedule.pdf")
 
 
 ggplot(monitor, aes(`Discharge Date`, `% TCM Order Placed Appropriately (Expect 100%)`)) + 
   geom_point() +
   geom_line()+
   geom_smooth(method = "lm", se = FALSE)
-ggsave("TCM-ord-placed.pdf")
+ggsave("~/Documents/safedata/tcm-quicc/TCM-ord-placed.pdf")
+
+
+
+##quick report
+nrow(master)
+sum(grepl("home", master$Disposition, ignore.case = T))
+t <- master[grepl("home", master$Disposition, ignore.case = T),]
+table(as.logical(t$`TCM Order`))
+table(t$`TCM Order`, t$Service)
+rm(t)
+
